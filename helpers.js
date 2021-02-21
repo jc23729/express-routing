57124a0c9c0af68c51c979abdea19a4026734906
Juan Cruz <jc23729@gmail.com>
2021-02-21 13:34:33 -0500

    findmode

--- helpers.js
+++ helpers.js
@@ -9,4 +9,23 @@ function createFrequencyCounter(arr) {
     }, {});
   }
 
+  /**
+ * Find the most common element in the array
+ * @param {Array} arr any array
+ */
+function findMode(arr) {
+    let freqCounter = createFrequencyCounter(arr);
+  
+    let count = 0;
+    let mostFrequent;
+  
+    for (let key in freqCounter) {
+      if (freqCounter[key] > count) {
+        mostFrequent = key;
+        count = freqCounter[key];
+      }
+    }
+  
+    return +mostFrequent;
+  }
   
