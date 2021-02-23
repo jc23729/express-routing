8163e7fe556a64a170e5b25de591992dcd57a503
Juan Cruz <jc23729@gmail.com>
2021-02-22 20:44:52 -0500

    update

--- app.js
+++ app.js
@@ -15,14 +15,25 @@ const { convertAndValidateNumsArray, findMode, findMean, findMedian } = require(
 //   value: 4
 // }
 
-app.get('/mean', function(req, res, next){
-    if (!req.query.nums){
-        throw new ExpressError('you must pass a query key of nums with a comma-separated list of numbers', 400)
+app.get('/mean', function(req, res, next) {
+    if (!req.query.nums) {
+      throw new ExpressError('You must pass a query key of nums with a comma-separated list of numbers.', 400)
     }
-}
-let numsAsStrings = req.query.nums.split(',');
-)
-
+    let numsAsStrings = req.query.nums.split(',');
+    // check if anything bad was put in
+    let nums = convertAndValidateNumsArray(numsAsStrings);
+    if (nums instanceof Error) {
+      throw new ExpressError(nums.message);
+    }
+  
+  
+    let result = {
+      operation: "mean",
+      result: findMean(nums)
+    }
+  
+    return res.send(result);
+  });
 
 
 // Our First Express App
